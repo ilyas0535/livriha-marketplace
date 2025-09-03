fdf0a2538bfffc7f5ea0cf9d01e65b5605f5ce38
ilyas0535 <protechdza@gmail.com>
2025-09-03 14:58:06 +0100

    Fix email timeout causing 502 error

--- monmagasin/settings.py
+++ monmagasin/settings.py
@@ -143,13 +143,17 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
 # Email configuration
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'protechdza@gmail.com'
-EMAIL_HOST_PASSWORD = 'pnbp msof iagp udzz'
-DEFAULT_FROM_EMAIL = 'protechdza@gmail.com'
+if DEBUG:
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+else:
+    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+    EMAIL_HOST = 'smtp.gmail.com'
+    EMAIL_PORT = 587
+    EMAIL_USE_TLS = True
+    EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', 'protechdza@gmail.com')
+    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', 'pnbp msof iagp udzz')
+    EMAIL_TIMEOUT = 10  # Add timeout
+DEFAULT_FROM_EMAIL = os.environ.get('DEFAULT_FROM_EMAIL', 'protechdza@gmail.com')
 
 AUTH_USER_MODEL = 'accounts.User'
 
