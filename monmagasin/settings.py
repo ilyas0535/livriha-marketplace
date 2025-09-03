380aa1dff9445ab72bd9367984c0a07b79e0e90f
ilyas0535 <protechdza@gmail.com>
2025-09-03 14:32:38 +0100

    Fix CSRF for custom domain

--- monmagasin/settings.py
+++ monmagasin/settings.py
@@ -27,7 +27,10 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-dyrnnp0zmdpwgvoi7p2k4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '*').split(',')
+
+# CSRF settings
+CSRF_TRUSTED_ORIGINS = os.environ.get('CSRF_TRUSTED_ORIGINS', '').split(',') if os.environ.get('CSRF_TRUSTED_ORIGINS') else []
 
 
 # Application definition
