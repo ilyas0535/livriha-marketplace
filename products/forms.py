e708544b097b4d7605174d58d5d63a9879b718f9
ilyas0535 <protechdza@gmail.com>
2025-09-05 21:13:11 +0100

    Add products list template

--- products/forms.py
+++ products/forms.py
@@ -1,9 +1,24 @@
 from django import forms
 from .models import Product, ProductImage
 
+class MultipleFileInput(forms.ClearableFileInput):
+    allow_multiple_selected = True
+
+class MultipleFileField(forms.FileField):
+    def __init__(self, *args, **kwargs):
+        kwargs.setdefault("widget", MultipleFileInput())
+        super().__init__(*args, **kwargs)
+
+    def clean(self, data, initial=None):
+        single_file_clean = super().clean
+        if isinstance(data, (list, tuple)):
+            result = [single_file_clean(d, initial) for d in data]
+        else:
+            result = single_file_clean(data, initial)
+        return result
+
 class ProductForm(forms.ModelForm):
-    images = forms.FileField(
-        widget=forms.FileInput(attrs={'multiple': True}),
+    images = MultipleFileField(
         required=False,
         help_text='Select multiple images for your product'
     )
@@ -20,8 +35,3 @@ class ProductForm(forms.ModelForm):
             'quantity': forms.NumberInput(attrs={'class': 'form-control'}),
             'low_stock_threshold': forms.NumberInput(attrs={'class': 'form-control'}),
         }
-
-class ProductImageForm(forms.ModelForm):
-    class Meta:
-        model = ProductImage
-        fields = ['image']
