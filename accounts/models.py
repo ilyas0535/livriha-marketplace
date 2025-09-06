70472b70993510873ad39a20fc022e5e23206bd1
ilyas0535 <protechdza@gmail.com>
2025-09-06 06:00:52 +0100

    Add missing verification_token field to User model

--- accounts/models.py
+++ accounts/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class User(AbstractUser):
     is_seller = models.BooleanField(default=False)
     email_verified = models.BooleanField(default=False)
+    verification_token = models.CharField(max_length=100, blank=True)
 
 class SupportMessage(models.Model):
     name = models.CharField(max_length=100)
